de4293362495d41996904a5de8f3023963257270
betomago09 <118900935+betomago09@users.noreply.github.com>
2025-06-07 17:02:09 +0100

    WIP: Save changes before rebase

--- SDTtest/src/main.cpp
+++ SDTtest/src/main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
+    for (int i = 0; i < numbers.length - 1; i++) {
+    for (int j = 0; j < numbers.length - i - 1; j++) {
+        if (numbers[j] > numbers[j + 1]) {
+            int temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+        }
+    }
+}
     int arr[5] = {1, 2, 3, 4, 5};
     for(int i : arr) cout << i << " ";
     return 0;
