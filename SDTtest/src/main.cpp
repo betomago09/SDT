30835cd007cacf34dde228dbc35ace89ca5553e4
betomago09 <118900935+betomago09@users.noreply.github.com>
2025-06-07 17:19:45 +0100

    New changes

--- SDTtest/src/main.cpp
+++ SDTtest/src/main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
-using namespace std;
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
 
 int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
-    for(int i : arr) cout << i << " ";
+    const int size = 10;
+    std::vector<int> numbers(size);
+
+    // Seed the random number generator
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    // Fill array with random numbers
+    for (int i = 0; i < size; ++i) {
+        numbers[i] = std::rand() % 100; // random values from 0 to 99
+    }
+
+    std::cout << "Unsorted array:\n";
+    for (int num : numbers) std::cout << num << " ";
+    std::cout << "\n";
+
+    // Bubble sort
+    for (int i = 0; i < size - 1; ++i) {
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (numbers[j] > numbers[j + 1]) {
+                std::swap(numbers[j], numbers[j + 1]);
+            }
+        }
+    }
+
+    std::cout << "Sorted array:\n";
+    for (int num : numbers) std::cout << num << " ";
+    std::cout << "\n";
+
+    // Sum of elements
+    int sum = 0;
+    for (int num : numbers) sum += num;
+    std::cout << "Sum of elements: " << sum << "\n";
+
     return 0;
 }
